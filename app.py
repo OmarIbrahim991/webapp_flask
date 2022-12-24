d5a2e9ce4b179c70116095cfdbe6f2079ebecdda
Omar Ibrahim <omar.ibrahim.991@gmail.com>
2022-12-24 21:08:32 +0200

    Add Flask SocketIO

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def main():
     return "<h1>Flask Web App</h1>"
+
+@socketio.on("initialize")
+def start():
+    socketio.emit("start", ["A", "B", "C"])
+    return ""
